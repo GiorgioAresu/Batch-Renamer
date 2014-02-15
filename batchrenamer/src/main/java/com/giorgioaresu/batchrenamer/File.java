c0cce0df305bd7e50e5d50a748b2b8fcd25f2018
GiorgioAresu <giorgioaresu@gmail.com>
2014-02-15 15:49:19 +0100

    Implemented intent filter for sharing files from other applications

--- batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/File.java
+++ batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/File.java
@@ -1,23 +1,33 @@
 package com.giorgioaresu.batchrenamer;
 
+import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
 
 public class File implements Parcelable {
+    Uri fileUri;
     String currentName;
     String newName;
 
     private File(Parcel in) {
+        fileUri = in.readParcelable((ClassLoader) Uri.CREATOR);
         currentName = in.readString();
         newName = in.readString();
     }
 
+    public File(Uri uri) {
+        fileUri = uri;
+        currentName = newName = fileUri.getLastPathSegment();
+    }
+
     public File(String name) {
-        newName = currentName = name;
+        this(Uri.parse(name));
     }
 
     // TODO: This constructor should be removed when file-picking system is implemented
     public File(String currentName, String newName) {
+        this(Uri.parse(currentName));
         this.currentName = currentName;
         this.newName = newName;
     }
@@ -25,6 +35,8 @@ public class File implements Parcelable {
     public int Rename() {
         // TODO: implement file rename
         try {
+            java.io.File file = new java.io.File(fileUri.getPath());
+            Log.d("file", "Name: " + file.getName() + ", Path: " + file.getAbsolutePath() + ", uriScheme: " + fileUri.getScheme() + ", canWrite: " + (file.canWrite() ? "true" : "false"));
             return 0;
         } catch (Exception e) {
             return 1;
@@ -49,6 +61,7 @@ public class File implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
+        Uri.writeToParcel(parcel, fileUri);
         parcel.writeString(currentName);
         parcel.writeString(newName);
     }
--- batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/MainActivity.java
+++ batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/MainActivity.java
@@ -3,29 +3,109 @@ package com.giorgioaresu.batchrenamer;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends Activity implements FileList_Fragment.FileFragmentInterface, ActionList_Fragment.OnActionSelectedListener, ActionEdit_Fragment.actionEditFragment_Callbacks {
 
     private ArrayList<File> mFiles;
+    private ArrayList<Uri> fileUris = new ArrayList<>();
     private ActionList_Fragment actionList_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
 
         if (savedInstanceState == null) {
 
         }
 
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        Toast.makeText(this, "type: " + type, Toast.LENGTH_LONG).show();
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            Parcelable parcelableExtra = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            if (parcelableExtra != null) {
+                Uri uri = decodeUri((Uri) parcelableExtra);
+                if (uri != null) {
+                    fileUris.add(uri);
+                } else {
+                    // Unsupported Uri scheme
+                    Toast.makeText(this, "Failed to get file path", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                // Unsupported stream, ie. text/plain
+                handleUnsupportedObject(intent);
+            }
+        } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+            ArrayList<Uri> parcelableArrayListExtra = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+            if (parcelableArrayListExtra != null) {
+                // Add files to list
+                Uri firstUri = parcelableArrayListExtra.get(0);
+                if (firstUri.equals(decodeUri(firstUri))) {
+                    fileUris.addAll(parcelableArrayListExtra);
+                } else {
+                    boolean filesSkipped = false;
+                    for (Uri uri : parcelableArrayListExtra) {
+                        uri = decodeUri(uri);
+                        if (uri != null) {
+                            fileUris.add(uri);
+                        } else {
+                            filesSkipped = true;
+                        }
+                    }
+                    if (filesSkipped) {
+                        // Unsupported Uri scheme
+                        Toast.makeText(this, "Failed to get some file path", Toast.LENGTH_LONG).show();
+                    }
+                }
+            } else {
+                // Unsupported stream, ie. text/plain
+                handleUnsupportedObject(intent);
+            }
+        } else {
+            // Handle other intents, such as being started from the home screen
+        }
+
+        setContentView(R.layout.activity_main);
 
         FragmentManager mFragmentManager = getFragmentManager();
         actionList_fragment = (ActionList_Fragment) mFragmentManager.findFragmentById(R.id.action_fragment);
+    }
+
+    private Uri decodeUri(Uri uri) {
+        String scheme = uri.getScheme();
+        if ("file".equals(scheme)) {
+            return uri;
+        } else if ("content".equals(scheme)) {
+            // Find in media store database the true reference to the file
+            String[] proj = {MediaStore.MediaColumns.DATA};
+            Cursor cursor = getContentResolver().query(uri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+            cursor.moveToFirst();
+            return Uri.parse("file:///" + cursor.getString(column_index));
+        } else {
+            Log.e(getLocalClassName(), "failed to recognize Uri: " + uri.toString());
+            return null;
+        }
+    }
+
+    private void handleUnsupportedObject(Intent intent) {
+        Toast.makeText(this, "Unsupported object, type: " + intent.getType(), Toast.LENGTH_LONG).show();
+        // TODO: Do something more intelligent
     }
 
     @Override
@@ -40,6 +120,11 @@ public class MainActivity extends Activity implements FileList_Fragment.FileFrag
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
+            case R.id.action_start:
+                for (File f : mFiles) {
+                    f.Rename();
+                }
+                return true;
             case R.id.action_settings:
                 return true;
             default:
@@ -52,16 +137,6 @@ public class MainActivity extends Activity implements FileList_Fragment.FileFrag
     }
 
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        //menu.setGroupVisible(R.id.menugroup_edit_action, ciao);
-
-        //menu.findItem(R.id.action_start).setVisible(!ciao);
-        //menu.findItem(R.id.action_settings).setVisible(!ciao);
-        //ciao=!ciao;
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
     public void onFileSelected(File file) {
         // TODO: Implement interface
     }
@@ -69,52 +144,24 @@ public class MainActivity extends Activity implements FileList_Fragment.FileFrag
     @Override
     public ArrayList<File> provideFiles() {
         mFiles = new ArrayList<>();
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
-        mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+        if (fileUris.isEmpty()) {
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+            mFiles.add(new File("Lost.3x01.Storia.Di.Due.Citta.ITA.DVDRip.XviD-NovaRip"));
+        } else {
+            for (Uri uri : fileUris) {
+                mFiles.add(new File(uri));
+            }
+        }
 
         return mFiles;
     }
