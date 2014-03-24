437ea7d12743f7aa0c8ac3895e07de557c1ef288
GiorgioAresu <giorgioaresu@gmail.com>
2014-03-24 14:01:20 +0100

    Extract ApplyTo radioGroup to a separate layout file

--- batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/actions/Add.java
+++ batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/actions/Add.java
@@ -21,9 +21,9 @@ public class Add extends Action {
     static final String KEY_APPLYTO = "ApplyTo";
 
     private enum ApplyTo {
-        NAME(R.id.action_add_radio_name),
-        EXTENSION(R.id.action_add_radio_extension),
-        BOTH(R.id.action_add_radio_both);
+        NAME(R.id.action_radio_name),
+        EXTENSION(R.id.action_radio_extension),
+        BOTH(R.id.action_radio_both);
 
         private final int id;
 
@@ -52,11 +52,11 @@ public class Add extends Action {
         public static int getStringResource(ApplyTo applyTo) {
             switch (applyTo) {
                 case NAME:
-                    return R.string.action_add_applyToName;
+                    return R.string.action_applyToName;
                 case EXTENSION:
-                    return R.string.action_add_applyToExtension;
+                    return R.string.action_applyToExtension;
                 default:
-                    return R.string.action_add_applyToBoth;
+                    return R.string.action_applyToBoth;
             }
         }
     }
@@ -140,7 +140,7 @@ public class Add extends Action {
             position = Integer.valueOf(mPosition.getText().toString());
             CheckBox mBackward = (CheckBox) view.findViewById(R.id.action_add_backward);
             backward = mBackward.isChecked();
-            RadioGroup mApplyTo = (RadioGroup) view.findViewById(R.id.action_add_radiogroup);
+            RadioGroup mApplyTo = (RadioGroup) view.findViewById(R.id.action_radiogroup);
             applyTo = ApplyTo.getValue(mApplyTo.getCheckedRadioButtonId());
         } catch (Exception e) {
             Log.e("updateDataFromView", "NPE");
@@ -165,7 +165,7 @@ public class Add extends Action {
             CheckBox mFromEnd = (CheckBox) view.findViewById(R.id.action_add_backward);
             mFromEnd.setChecked(backward);
 
-            RadioGroup mApplyTo = (RadioGroup) view.findViewById(R.id.action_add_radiogroup);
+            RadioGroup mApplyTo = (RadioGroup) view.findViewById(R.id.action_radiogroup);
             mApplyTo.check(applyTo.getID());
         } catch (Exception e) {
             Log.e("updateViewFromData", "NPE");
@@ -185,7 +185,7 @@ public class Add extends Action {
                 + context.getString(R.string.action_add_index) + ": " + checkForEmpty(String.valueOf(position)) + ". "
                 + context.getString(R.string.action_add_backward) + ": "
                 + context.getString(backward ? R.string.true_ : R.string.false_) + ". "
-                + context.getString(R.string.action_add_apply) + ": " + context.getString(ApplyTo.getStringResource(applyTo));
+                + context.getString(R.string.action_apply) + ": " + context.getString(ApplyTo.getStringResource(applyTo));
         return str;
     }
 
