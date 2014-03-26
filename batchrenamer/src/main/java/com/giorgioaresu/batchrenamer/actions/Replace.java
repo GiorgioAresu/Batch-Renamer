8fde21d7e04693e167a2d9bffb291780f7ece395
GiorgioAresu <giorgioaresu@gmail.com>
2014-03-26 15:41:54 +0100

    Add regex checkbox to Replace layout

--- batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/actions/Replace.java
+++ batchrenamer/src/main/java/com/giorgioaresu/batchrenamer/actions/Replace.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Parcel;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -15,10 +16,12 @@ import org.json.JSONObject;
 
 public class Replace extends Action {
     static final String KEY_TARGET = "Target";
+    static final String KEY_REGEX = "Regex";
     static final String KEY_REPLACEMENT = "Replacement";
     static final String KEY_APPLYTO = "ApplyTo";
 
     String target = "";
+    Boolean regex = false;
     String replacement = "";
     ApplyTo applyTo = ApplyTo.BOTH;
 
@@ -45,6 +48,9 @@ public class Replace extends Action {
             EditText mTarget = (EditText) view.findViewById(R.id.action_replace_target);
             target = mTarget.getText().toString();
 
+            CheckBox mRegex = (CheckBox) view.findViewById(R.id.action_replace_regex);
+            regex = mRegex.isChecked();
+
             EditText mReplacement = (EditText) view.findViewById(R.id.action_replace_replacement);
             replacement = mReplacement.getText().toString();
 
@@ -67,6 +73,9 @@ public class Replace extends Action {
             EditText mTarget = (EditText) view.findViewById(R.id.action_replace_target);
             mTarget.setText(target);
 
+            CheckBox mRegex = (CheckBox) view.findViewById(R.id.action_replace_regex);
+            mRegex.setChecked(regex);
+
             EditText mReplacement = (EditText) view.findViewById(R.id.action_replace_replacement);
             mReplacement.setText(replacement);
 
@@ -87,6 +96,7 @@ public class Replace extends Action {
     protected String getContentDescription() {
         String str;
         str = context.getString(R.string.action_replace_target) + ": " + checkForEmpty(target) + ". "
+                + context.getString(R.string.action_regex) + ": " + getValueToString(regex) + ". "
                 + context.getString(R.string.action_replace_replacement) + ": " + checkForEmpty(replacement) + ". "
                 + context.getString(R.string.action_apply) + ": " + ApplyTo.getLabel(context, applyTo);
         return str;
@@ -99,6 +109,7 @@ public class Replace extends Action {
     protected JSONObject serializeToJSON() throws JSONException {
         JSONObject jObject = new JSONObject();
         jObject.put(KEY_TARGET, target);
+        jObject.put(KEY_REGEX, regex);
         jObject.put(KEY_REPLACEMENT, replacement);
         jObject.put(KEY_APPLYTO, applyTo);
         return jObject;
@@ -110,6 +121,7 @@ public class Replace extends Action {
     @Override
     protected void deserializeFromJSON(JSONObject jObject) throws JSONException {
         target = jObject.getString(KEY_TARGET);
+        regex = jObject.getBoolean(KEY_REGEX);
         replacement = jObject.getString(KEY_REPLACEMENT);
         applyTo = ApplyTo.getValue(jObject.getInt(KEY_APPLYTO));
     }
@@ -120,6 +132,7 @@ public class Replace extends Action {
     @Override
     protected void createFromParcel(Parcel in) {
         target = in.readString();
+        regex = toBoolean(in.readByte());
         replacement = in.readString();
         applyTo = ApplyTo.getValue(in.readInt());
     }
@@ -130,6 +143,7 @@ public class Replace extends Action {
     @Override
     public void dumpToParcel(Parcel parcel, int i) {
         parcel.writeString(target);
+        parcel.writeByte(toByte(regex));
         parcel.writeString(replacement);
         parcel.writeInt(applyTo.getID());
     }
